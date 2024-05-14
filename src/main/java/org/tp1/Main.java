0ef196b3a086ea581d7b42bbefa4f9a0ce55affb
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-05-14 12:50:41 +0100

    Add phone number validation

--- src/main/java/org/tp1/Main.java
+++ src/main/java/org/tp1/Main.java
@@ -8,10 +8,10 @@ public final class Main {
         System.out.println("Hello and welcome!");
 
         try {
-            var w1 = new Worker("joao", Role.MANAGER, 1000, new Contact("ola@mecanicos.pt", 3123213));
+            var w1 = new Worker("joao", Role.MANAGER, 1000, new Contact("ola@mecanicos.pt", 928155170));
             w1.print();
 
-            var c1 = new Client("lucas", new NIF("999999990"), new Contact("lucas@email.com", 123));
+            var c1 = new Client("lucas", new NIF("999999990"), new Contact("lucas@email.com", 111111111));
             c1.print();
 
             var r1 = new Repair(new CarRegistration("11-11-AA"), VehicleType.CAR, w1.getId(), c1.getId(), "mudar oleo", 100, LocalDateTime.now(), LocalDateTime.now().plusWeeks(2));
