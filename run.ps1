ba94a839785df3490073e70138f6b4c374a51bd3
Lucas Linhares <lucasdelinhares@gmail.com>
2024-05-17 22:26:08 +0100

    Build scripts now by default build the rust library on debug mode and can accept a --release flag for release mode

--- run.ps1
+++ run.ps1
@@ -1,10 +1,16 @@
+param(
+    [switch]$release = $false
+)
+
+$buildFolder = if ($release) { "release" } else { "debug" }
+
 cd terminal_utils
-cargo build --release
+cargo build $(if ($release) { "--release" })
 
 cd ../TP1
 ./gradlew.bat installDist
 
-cp ../terminal_utils/target/release/terminal_utils.dll build/install/TP1/bin
+cp ../terminal_utils/target/$buildFolder/terminal_utils.dll build/install/TP1/bin
 
 cd build/install/TP1/bin
 ./TP1
--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "--release" ] || [ "$1" == "-r" ]; then
+    BUILD_TYPE="release"
+    CARGO_FLAG="--release"
+else
+    BUILD_TYPE="debug"
+    CARGO_FLAG=""
+fi
+
 cd terminal_utils
-cargo build --release
+cargo build $CARGO_FLAG
 
 cd ../TP1
 ./gradlew installDist
 
-cp ../terminal_utils/target/release/terminal_utils build/install/TP1/bin
+cp ../terminal_utils/target/$BUILD_TYPE/terminal_utils build/install/TP1/bin
 
 cd build/install/TP1/bin
 LD_LIBRARY_PATH="." ./TP1
